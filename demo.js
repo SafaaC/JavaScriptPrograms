9c315632a7c8e966e03c61a6e3e3553cc30a38ea
SafaaC <safaaacherak@gmail.com>
2023-08-18 18:40:27 +0530

    InitialCommit

---
+++ demo.js
@@ -0,0 +1,43 @@
+
+// function a(){
+//     let b=10;
+//     c();
+//     function c(){
+//         let d=125;
+//         console.log(b);
+//     }
+// }
+// a();
+// console.log(d);
+// let d=5;
+
+// {
+//     let a=1;
+//     const b=2;
+//     var c=3;
+// }
+// console.log(c);
+// console.log(a);
+// console.log(b);
+
+// var c=10;
+// {
+//     let a=1;
+//     const b=2;
+//     var c=3;
+//     console.log(c);
+//     console.log(a);
+//     console.log(b);
+// }
+// console.log(c);
+
+
+function a(){
+    let b=10;
+    c();
+    function c(){
+        let d=125;
+        console.log(b);
+    }
+}
+a();
---
+++ timeout.js
@@ -0,0 +1,14 @@
+// for(var i=1;i<=5;i++){
+//     setTimeout(function(){
+//         console.log(i);
+//     },i*1000);
+// }
+console.log("hi");
+setTimeout(function(){
+           console.log("hello");
+       },1);
+console.log("hi");
+console.log("hi");
+console.log("hi");
+console.log("hi");
+console.log("hi");
